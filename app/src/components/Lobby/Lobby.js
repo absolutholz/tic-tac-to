248a3eebaa84e502bf73e7cd526b3ba8468ad046
absolutholz <absolutholz@gmail.com>
2021-05-19 19:24:32 +0200

    Update pubnub lobby

--- app/src/components/Lobby/Lobby.js
+++ app/src/components/Lobby/Lobby.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+
 import { usePubNub } from 'pubnub-react';
 import { nanoid } from 'nanoid';
+
+import ButtonList from './../ButtonList';
 
 const Lobby = ({ username }) => {
 	// const [lobbyChannel, setLobbyChannel] = useState(null);
@@ -8,8 +11,13 @@ const Lobby = ({ username }) => {
 	let lobbyChannel = null;
 	let gameChannel = null;
 
-	const [players, setPlayers] = useState([]);
+	// const [players, setPlayers] = useState([]);
+	let piece = '';
+	// const [isPlaying, setIsPlaying] = useState(false);
+	// const [isDisabled, setIsDisabled] = useState(false);
 	const [isRoomCreator, setIsRoomCreator] = useState(false);
+	const [isWaitingForOpponent, setIsWaitingForOpponent] = useState(false);
+	const [shareableRoomId, setShareableRoomId] = useState('');
 
 	const pubnub = usePubNub();
 
@@ -19,7 +27,7 @@ const Lobby = ({ username }) => {
 				console.log({ msg });
 				// Start the game once an opponent joins the channel
 				if (msg.message.notRoomCreator) {
-					console.log('start the game');
+					alert(`Ready to start the game as \r\n Player ${ piece }`);
 					// Create a different channel for the game
 					gameChannel = `tictactoegame--${this.roomId}`;
 					pubnub.subscribe({
@@ -35,11 +43,34 @@ const Lobby = ({ username }) => {
 		});
 	};
 
+	// Create a room channel
+	const onPressCreate = (event) => {
+		// Create a random name for the channel
+		roomId = nanoid(5);
+		lobbyChannel = `tictactoelobby--${roomId}`; // Lobby channel name
+		setShareableRoomId(roomId);
+
+		setIsWaitingForOpponent(true);
+		setIsRoomCreator(true);
+		piece = 'X';
+
+		listen();
+
+		// setPlayers([
+		// 	{
+		// 		uuid: username,
+		// 	}
+		// ]);
+	};
+
 	// Join a room channel
-	const joinRoom = (roomIdToJoin) => {
-		roomId = roomIdToJoin;
-		lobbyChannel = `tictactoelobby--${roomId}`;
-		// setLobbyChannel(`tictactoelobby--${roomId}`, () => {
+	const onPressJoin = (event) => {
+		const roomIdToJoin = prompt('Enter the room id', '');
+
+		// Check if the user typed a value in the input field
+		if (roomIdToJoin) {
+			roomId = roomIdToJoin;
+			lobbyChannel = `tictactoelobby--${roomId}`;
 			// Check the number of people in the channel
 			pubnub.hereNow({
 				channels: [lobbyChannel],
@@ -51,6 +82,8 @@ const Lobby = ({ username }) => {
 					// }
 					// ]);
 					listen();
+
+					piece = 'O';
 
 					pubnub.publish({
 						message: {
@@ -65,32 +98,16 @@ const Lobby = ({ username }) => {
 			}).catch((error) => {
 				console.log(error);
 			});
-		// });
+		}
 	};
 
-	// Create a room channel
-	const onPressCreate = (event) => {
-		// Create a random name for the channel
-		roomId = nanoid(5);
-		// setLobbyChannel(`tictactoelobby--${roomId}`); // Lobby channel name
-		lobbyChannel = `tictactoelobby--${roomId}`; // Lobby channel name
-		alert(`Share this room ID with your friend\r\n${roomId}`);
-		setIsRoomCreator(true);
-		listen();
-		// setPlayers([
-		// 	{
-		// 		uuid: username,
-		// 	}
-		// ]);
-	};
-
-	// The 'Join' button was pressed
-	const onPressJoin = (event) => {
-		const roomIdToJoin = prompt('Enter the room id', '');
-
-		// Check if the user typed a value in the input field
-		if (roomIdToJoin) {
-			joinRoom(roomIdToJoin)
+	const shareLobbyRoomId = () => {
+		if (navigator.share) {
+			navigator.share({
+				text: shareableRoomId,
+			});
+		} else {
+			alert(`Share this room ID with your friend\r\n${shareableRoomId}`);
 		}
 	};
 
@@ -118,21 +135,72 @@ const Lobby = ({ username }) => {
 
 	return (
 		<div className="lobby">
-			<button
-				className="create-button "
-				// disabled={state.isDisabled}
-				onClick={() => onPressCreate()}
-			>
-				Create
-			</button>
-			<button
-				className="join-button"
-				onClick={() => onPressJoin()}
-			>
-				Join
-			</button>
 
-			<section>
+			{ !isWaitingForOpponent &&
+				<ButtonList>
+					<button
+						className="button"
+						onClick={() => onPressCreate()}
+					>
+						Create a game
+					</button>
+					<button
+						className="button"
+						onClick={() => onPressJoin()}
+					>
+						Join a game
+					</button>
+				</ButtonList>
+			}
+
+			{ isWaitingForOpponent &&
+				<div>
+					<p>Share this code with your friend <span>{ shareableRoomId }</span>
+					</p>
+					<ButtonList>
+						<button
+							className="button"
+							onClick={() => onPressCreate()}
+						>
+							Create a game
+						</button>
+						<button
+							className="button"
+							onClick={() => onPressJoin()}
+						>
+							Join a game
+						</button>
+					</ButtonList>
+				</div>
+			}
+
+
+
+
+
+
+			{/* { !isWaitingForPlayers &&
+				<div>
+					You are Player { piece }
+					<button
+					>
+						Start game
+					</button>
+				</div>
+			} */}
+
+			{/* { shareableRoomId &&
+				<section>
+					<h2>Share this code with your opponent</h2>
+					<button
+						onClick={ shareLobbyRoomId }
+					>
+						{ shareableRoomId }
+					</button>
+				</section>
+			} */}
+
+			{/* <section>
 				<h2>Players</h2>
 				<div>User: {username}</div>
 				<ol>
@@ -140,9 +208,8 @@ const Lobby = ({ username }) => {
 						players.map((player) => <li key={player.uuid}>{player.uuid}</li>)
 					}
 				</ol>
-			</section>
+			</section> */}
 		</div>
-
 	);
 };
 
